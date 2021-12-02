133b1099a5d86e0ae7311bbbc01f0f120f846383
mwcullen <mwcullen@asu.edu>
2021-12-02 00:39:30 -0700

    add day 2 soln

---
+++ py_backend_aoc2021/day_solns/day_02/day_02.py
@@ -0,0 +1,52 @@
+from typing import List, NamedTuple
+
+
+class Instruction (NamedTuple):
+    direction: str
+    val: int
+
+
+class Coordinates(NamedTuple):
+    hori: int
+    vert: int
+    aim: int
+
+
+def coordinateUpdate(i: Instruction, p: Coordinates) -> Coordinates:
+    if i.direction == 'forward':
+        newCoords = Coordinates(p.hori+i.val, p.vert + p.aim * i.val, p.aim)
+    elif i.direction == 'up':
+        newCoords = Coordinates(p.hori, p.vert, p.aim - i.val)
+    elif i.direction == 'down':
+        newCoords = Coordinates(p.hori, p.vert, p.aim + i.val)
+    else:
+        newCoords = p
+
+    return newCoords
+
+
+def main():
+    f = open("./input1.txt", "r", encoding='utf8')
+
+    instructionsRaw = f.read()
+
+    lstRawInstructions:List[str] = instructionsRaw.splitlines()
+
+    parsedInstructions = [Instruction]
+
+    for instruction in lstRawInstructions:
+        dirvalpair = instruction.split()
+
+        parsedInstructions.append(Instruction(
+            dirvalpair[0], int(dirvalpair[1])))
+
+    baseCoords = Coordinates(0, 0, 0)
+
+    for instruction in parsedInstructions:
+        baseCoords = coordinateUpdate(instruction, baseCoords)
+
+    print(baseCoords.vert * baseCoords.hori)
+
+
+if __name__ == '__main__':
+    main()
