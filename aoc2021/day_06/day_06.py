438cfb17c81c216d3a7c8f3944d48047519c5d35
mwcullen <mwcullen@asu.edu>
2021-12-06 17:53:27 -0700

    prep day 6

---
+++ aoc2021/day_06/day_06.py
@@ -0,0 +1,14 @@
+from typing import List, NamedTuple, Tuple
+from enum import Enum
+from dataclasses import dataclass
+
+
+def main():
+    f = open("./input1.txt", "r", encoding='utf8')
+
+    rawString = f.read()
+
+
+
+if __name__ == '__main__':
+    main()
