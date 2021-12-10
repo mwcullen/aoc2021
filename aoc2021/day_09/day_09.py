b5986adfd99438131529895c277ef4a4d68d8641
mwcullen <mwcullen@asu.edu>
2021-12-09 23:45:28 -0700

    prep day9

---
+++ aoc2021/day_09/day_09.py
@@ -0,0 +1,14 @@
+from typing import List
+from dataclasses import dataclass
+from typing import Dict
+
+
+def main():
+    f = open("./input1.txt", "r", encoding='utf8')
+
+    rawString = f.read()
+
+
+
+if __name__ == '__main__':
+    main()
